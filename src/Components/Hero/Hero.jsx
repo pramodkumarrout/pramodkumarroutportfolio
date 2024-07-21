c93b937ec5adb503052a2b16d44417408a8c160f
Pramod Kumar Rout <pramodkumarrout243@gmail.com>
2024-07-21 16:30:11 +0530

    hero section structure complete

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () => {
+  return (
+    <div className = 'hero'>
+       <img src= {profile_img} alt = "" />
+       <h1> Hi,  I’m Pramod Kumar Rout </h1>
+        <p> A Full Stack Developer</p>
+        <div className = "hero-action">
+            <div className = "hero-connect"> Connect with me </div>
+            <div className = "hero-resume"> My Resume </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
