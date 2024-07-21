9766046cd84539c7b402ab6414dceb61271bb7a0
Pramod Kumar Rout <pramodkumarrout243@gmail.com>
2024-07-21 19:33:11 +0530

    skills section style completed

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -22,16 +22,16 @@ const About = () => {
                 <p> Proficient in modern web technologies including HTML5, CSS3, JavaScript, and framework React.js. Design interactive, responsive, and cross-browser compatible websites. Committed to staying updated with the latest trends and best practices in web development to deliver high-quality and performance-optimized solutions.</p>
                 </div>
                 <div className = "about-skills">
-                    <div className ="about-skill"> <p> HTML & CSS </p> <hr style={ {width:"90%"} } /> </div>
+                    <div className ="about-skill"> <p> HTML & CSS </p> <hr style={ {width:"70%"} } /> </div>
                     <div className ="about-skill"> <p> React JS </p> <hr style={ {width:"70%"} } /> </div>
                     <div className ="about-skill"> <p> JavaScript </p> <hr style={ {width:"60%"} } /> </div>
-                    <div className ="about-skill"> <p> Next JS </p> <hr style={ {width:"50%"} } /> </div>
+                    <div className ="about-skill"> <p> Next JS </p> <hr style={ {width:"70%"} } /> </div>
 
                 </div>
             </div>
         </div>
 
-        <div className ="about-achievemnets">
+        <div className ="about-achievements">
             <div className = "about-achievement">
                 <h1> 1+</h1>
                 <p> YEARS OF EXPERIENCE </p>
